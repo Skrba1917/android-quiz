a4235065bf662a2cab3152113e19d057da9e611e
unknown <nenad.skrbic1917@gmail.com>
2023-04-21 14:57:10 +0200

    second game gui

--- app/src/main/java/com/example/androidquiz/FirstGameActivity.java
+++ app/src/main/java/com/example/androidquiz/FirstGameActivity.java
@@ -2,13 +2,32 @@ package com.example.androidquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstGameActivity extends AppCompatActivity {
+    private Button btnNextGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_game);
+
+        btnNextGame = (Button) findViewById(R.id.btn_nextGame);
+        btnNextGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondGameActivity();
+            }
+        });
+
     }
+
+    public void openSecondGameActivity() {
+        Intent intent = new Intent(this, SecondGameActivity.class);
+        startActivity(intent);
+    }
+
 }
