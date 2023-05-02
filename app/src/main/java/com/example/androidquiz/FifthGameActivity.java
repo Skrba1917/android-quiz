2456a276047a8c29a0bb0569813a24b7779792cd
unknown <nenad.skrbic1917@gmail.com>
2023-05-02 19:00:18 +0200

    finished gui for all games

--- app/src/main/java/com/example/androidquiz/FifthGameActivity.java
+++ app/src/main/java/com/example/androidquiz/FifthGameActivity.java
@@ -1,14 +1,33 @@
 package com.example.androidquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FifthGameActivity extends AppCompatActivity {
 
+    private Button btnNextGame;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth_game);
+
+        btnNextGame = (Button) findViewById(R.id.btn_nextGame);
+        btnNextGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSixthGameActivity();
+            }
+        });
+
+    }
+
+    public void openSixthGameActivity() {
+        Intent intent = new Intent(this, SixthGameActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/androidquiz/SixthGameActivity.java
@@ -0,0 +1,34 @@
+package com.example.androidquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SixthGameActivity extends AppCompatActivity {
+
+    private Button submitButton;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sixth_game);
+
+        submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToHome();
+            }
+        });
+    }
+
+    public void backToHome() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+}
